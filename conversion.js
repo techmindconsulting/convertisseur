1ba1d9496d1d69f1f5f7c97cbc8c913a6b2122bd
sfinx13 <saidi.ahamada@gmail.com>
2021-02-07 17:46:43 +0100

    update

--- conversion.js
+++ conversion.js
@@ -10,19 +10,9 @@ function convertir() {
 
     const rateSelect = document.getElementById('rate');
     const rate = rateSelect.value;
-    
-    console.log(rateSelect.options);
-
     const currency = rateSelect.options[rateSelect.selectedIndex].text;
-    
     const resultat = amount * rate;
     
     document.getElementById('resultat').innerText = resultat.toFixed(2) +  ` ${currency}`;
 
-    // Appliquer style CSS à l'aide de la classe
- /*   document.querySelectorAll('p.result').forEach( el => {
-        el.style.border =  "3px solid green";
-        el.style.margin = "10px 0";
-    });*/
-    
 }
